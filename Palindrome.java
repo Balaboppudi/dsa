7ff4d7ba757ece780ab803547285b27c876c9c0f
bala <subbu509@gmail.com>
2026-01-04 13:27:41 -0500

    palindrom

--- Palindrome.java
+++ Palindrome.java
@@ -1,12 +1,27 @@
 public class Palindrome {
 
     public static void main(String[] args) {
-        printIsPalindrome("ABCDCBA");
-                printIsPalindrome("TEST");
+        // printIsPalindrome("ABCDCBA");
+        //         printIsPalindrome("TEST");
+
+printIsPalindrome2("ABCDCBA");
+                printIsPalindrome2("TEST");
 
     }
     private static void printIsPalindrome(String s){
            
         System.out.println(new StringBuffer(s).reverse().toString().equals(s) ? "YES":"NO");
+    }
+
+    private static void printIsPalindrome2(String s){
+        for(int i=0,j=s.length()-1;i<=j;i++,j--){
+            if(s.charAt(i) !=s.charAt(j)){
+                System.out.println("NO");
+                return;
+            }
+
+        }
+        System.out.println("YES");
+
     }
 }
